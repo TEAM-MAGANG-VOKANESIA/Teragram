b1a87ae55b22bafb85be48de10c14854376e8a56
KOKRendy <rendyatika20@gmail.com>
2023-06-22 16:02:36 +0700

    fix overflow responsive issue

--- resources/views/components/landing/app.blade.php
+++ resources/views/components/landing/app.blade.php
@@ -50,8 +50,8 @@
 
             <hr class="w-full h-px my-5 bg-gray-300  dark:bg-gray-500">
 
-            <div>
-                <ul class="text-xl space-y-4 p-9">
+            <div class="flex justify-center">
+                <ul class="text-xl space-y-3">
                     <li class="flex items-center space-x-4">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">

8ee75235e1c5f8b4f54dcccf8f6d151ee3fe598f
Agthzz <agathaanandra010@gmail.com>
2023-06-23 15:21:27 +0700

    feature notification

--- resources/views/components/landing/app.blade.php
+++ resources/views/components/landing/app.blade.php
@@ -50,7 +50,7 @@
 
             <hr class="w-full h-px my-5 bg-gray-300  dark:bg-gray-500">
 
-            <div>
+            <div id="sidebar">
                 <ul class="text-xl space-y-4 p-9">
                     <li class="flex items-center space-x-4">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -70,16 +70,16 @@
                             Explore
                         </a>
                     </li>
-                    <li class="flex items-center space-x-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
-                        </svg>
-                        <a href="">
-                            Notification
-                        </a>
-                    </li>
+                    <div id="notification">
+                        <li class="flex items-center space-x-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+                            </svg>
+                            <a href="" onclick="notification()">Notification</a>
+                        </li>
+                    </div>
                     <li class="flex items-center space-x-4">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="w-6 h-6">

e75693676ba9ea9a7984d5a6034e576bc9ec038b
KOKRendy <rendyatika20@gmail.com>
2023-06-26 13:59:44 +0700

    refactor branch conflict

--- resources/views/components/landing/app.blade.php
+++ resources/views/components/landing/app.blade.php
@@ -162,21 +162,6 @@
         </div>
         {{ $slot }}
     </div>
-    <script>
-        var moreButton = document.getElementById('moreButton');
-        var moreDropdownMenu = document.getElementById('moreMenus');
-        var moreIsDropdownVisible = false;
-
-        moreButton.addEventListener('click', function() {
-            if (moreIsDropdownVisible) {
-                moreDropdownMenu.classList.add('hidden');
-                moreIsDropdownVisible = false;
-            } else {
-                moreDropdownMenu.classList.remove('hidden');
-                moreIsDropdownVisible = true;
-            }
-        })
-    </script>
 </body>
 
 </html>

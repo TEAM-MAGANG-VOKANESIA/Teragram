2ef77a814d6f2b1c2aea9601954477b3d7039909
Agthzz <agathaanandra010@gmail.com>
2023-06-26 16:42:19 +0700

    8p

--- resources/views/components/landing/app.blade.php
+++ resources/views/components/landing/app.blade.php
@@ -214,20 +214,6 @@
                 isNotificationShow = true;
             }
         })
-
-        var moreButton = document.getElementById('moreButton');
-        var moreDropdownMenu = document.getElementById('moreMenus');
-        var moreIsDropdownVisible = false;
-
-        moreButton.addEventListener('click', function() {
-            if (moreIsDropdownVisible) {
-                moreDropdownMenu.classList.add('hidden');
-                moreIsDropdownVisible = false;
-            } else {
-                moreDropdownMenu.classList.remove('hidden');
-                moreIsDropdownVisible = true;
-            }
-        })
     </script>
 </body>
 

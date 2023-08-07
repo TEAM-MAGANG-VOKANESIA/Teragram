adebc427386e5dde69e28c7d188091140ac23055
KOKRendy <rendyatika20@gmail.com>
2023-08-07 20:22:42 +0700

    refactor code

--- app/Http/Controllers/API/V1/HomeController.php
+++ app/Http/Controllers/API/V1/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest('id')->get();
+        $posts = Post::with('user')->latest('id')->get();
         return response()->json($posts);
     }
 }

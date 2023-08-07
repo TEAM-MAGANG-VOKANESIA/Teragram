d98606894785b9be36f955901369a815b8f4f3df
KOKRendy <rendyatika20@gmail.com>
2023-08-07 19:56:59 +0700

    Add API homepage

---
+++ app/Http/Controllers/API/V1/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest('id')->get();
+        return response()->json($posts);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\V1\Auth\LoginController;
+use App\Http\Controllers\API\V1\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::controller(LoginController::class)->prefix('/v1')->group(function () {
     Route::post('/login/store', 'store')->name('login.store.api.index');
+});
+
+Route::controller(HomeController::class)->prefix('/v1')->group(function () {
+    Route::get('/home', 'index')->name('home.api.index');
 });

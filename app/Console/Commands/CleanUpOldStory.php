71daaddf896577b16f6e4f26b3911bce519b4294
KOKRendy <rendyatika20@gmail.com>
2023-08-30 13:16:40 +0700

    add feature command story

---
+++ app/Console/Commands/CleanUpOldStory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Story;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class CleanUpOldStory extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clean-up-old-story';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clean up story after 24 hours';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $twentyHoursAgo = Carbon::now()->subHours(24);
+
+        $story = Story::where('created_at', '<', $twentyHoursAgo);
+
+        Storage::disk('public')->delete($story->image);
+        
+        $story->delete();
+
+        $this->info('Old stories clean up successfull');
+    }
+}

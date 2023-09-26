cedb074a74b9adc1162ace957d2d1dede23175cd
KOKRendy <rendyatika20@gmail.com>
2023-09-26 18:19:46 +0700

    refactor follow controller and make follow service

--- app/Http/Controllers/API/V1/FollowController.php
+++ app/Http/Controllers/API/V1/FollowController.php
@@ -3,63 +3,31 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
-use App\Models\Follow;
-use Illuminate\Support\Facades\DB;
+use App\Services\FollowService;
 
 class FollowController extends Controller
 {
-    public function followUser(string $userId)
+    public function followUser(string $userId, FollowService $followService)
     {
-        DB::beginTransaction();
         try {
-            $alreadyFollowing = Follow::where('user_id', auth()->id())->where('followed_user_id', $userId)->first();
-            if ($alreadyFollowing) {
-                return [
-                    'success' => false,
-                    'message' => 'You already following this user',
-                ];
-            }
-            Follow::create([
-                'user_id' => auth()->id(),
-                'followed_user_id' => $userId,
-            ]);
-            DB::commit();
-            return [
-                'success' => true,
-                'message' => 'Successfull follow user',
-            ];
+            $followResponse = $followService->followUser($userId);
+            return response()->json($followResponse);
         } catch (\Exception $e) {
-            DB::rollBack();
             return [
                 'success' => false,
-                'message' => 'Service Error',
                 'errors' => $e->getMessage(),
             ];
         }
     }
 
-    public function unfollowUser(string $userId)
+    public function unfollowUser(string $userId, FollowService $followService)
     {
-        DB::beginTransaction();
         try {
-            $isFollowing = Follow::where('user_id', auth()->id())->where('followed_user_id', $userId)->first();
-            if ($isFollowing === null) {
-                return [
-                    'success' => false,
-                    'message' => 'You\'re not following this user',
-                ];
-            }
-            $isFollowing->delete();
-            DB::commit();
-            return [
-                'success' => true,
-                'message' => 'Successfull unfollow user',
-            ];
+            $followResponse = $followService->unfollowUser($userId);
+            return response()->json($followResponse);
         } catch (\Exception $e) {
-            DB::rollBack();
             return [
                 'success' => false,
-                'message' => 'Service Error',
                 'errors' => $e->getMessage(),
             ];
         }
---
+++ app/Services/FollowService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Follow;
+use Illuminate\Support\Facades\DB;
+
+class FollowService
+{
+    public function followUser(string $userId)
+    {
+        DB::beginTransaction();
+        try {
+            $alreadyFollowing = Follow::where('user_id', auth()->id())->where('followed_user_id', $userId)->first();
+            if ($alreadyFollowing) {
+                return [
+                    'success' => false,
+                    'message' => 'You already following this user',
+                ];
+            }
+            Follow::create([
+                'user_id' => auth()->id(),
+                'followed_user_id' => $userId,
+            ]);
+            DB::commit();
+            return [
+                'success' => true,
+                'message' => 'Successfull follow user',
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return [
+                'success' => false,
+                'message' => 'Service Error',
+                'errors' => $e->getMessage(),
+            ];
+        }
+    }
+
+    public function unfollowUser(string $userId)
+    {
+        DB::beginTransaction();
+        try {
+            $isFollowing = Follow::where('user_id', auth()->id())->where('followed_user_id', $userId)->first();
+            if ($isFollowing === null) {
+                return [
+                    'success' => false,
+                    'message' => 'You\'re not following this user',
+                ];
+            }
+            $isFollowing->delete();
+            DB::commit();
+            return [
+                'success' => true,
+                'message' => 'Successfull unfollow user',
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return [
+                'success' => false,
+                'message' => 'Service Error',
+                'errors' => $e->getMessage(),
+            ];
+        }
+    }
+}
